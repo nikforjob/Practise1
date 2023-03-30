5d514a52a00bdddbfdfee2d133e98fff9386b5ee
Nikhil.Gupta <Nikhil.Gupta@PRO-5CG11060DG.PROFINCHBLR.COM>
2023-03-30 17:22:58 +0530

    uyhgf

--- Practsie1/src/Practise1/Practise1.java
+++ Practsie1/src/Practise1/Practise1.java
@@ -5,6 +5,7 @@ public class Practise1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Nikhil Is doing Selenium project");
+		System.out.println("Nikhil Is doing Selenium project");
 
 	}
 
